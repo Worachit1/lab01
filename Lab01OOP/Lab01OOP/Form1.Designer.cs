cf930b3af2bee854da58b2e7182aa3810c7815c0
Worachit1 <worachit.t@kkumail.com>
2022-12-25 16:54:17 +0700

    calculator

---
+++ Lab01OOP/Lab01OOP/Form1.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace Lab01OOP
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numseven = new System.Windows.Forms.Button();
+            this.Input = new System.Windows.Forms.TextBox();
+            this.numeight = new System.Windows.Forms.Button();
+            this.numnine = new System.Windows.Forms.Button();
+            this.numfour = new System.Windows.Forms.Button();
+            this.numfive = new System.Windows.Forms.Button();
+            this.numsix = new System.Windows.Forms.Button();
+            this.numone = new System.Windows.Forms.Button();
+            this.numtwo = new System.Windows.Forms.Button();
+            this.numthree = new System.Windows.Forms.Button();
+            this.numzero = new System.Windows.Forms.Button();
+            this.numsum = new System.Windows.Forms.Button();
+            this.numpush = new System.Windows.Forms.Button();
+            this.numminus = new System.Windows.Forms.Button();
+            this.nummultiply = new System.Windows.Forms.Button();
+            this.numdivision = new System.Windows.Forms.Button();
+            this.delete = new System.Windows.Forms.Button();
+            this.point = new System.Windows.Forms.Button();
+            this.process = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // numseven
+            // 
+            this.numseven.BackColor = System.Drawing.SystemColors.Info;
+            this.numseven.Font = new System.Drawing.Font("Segoe UI Emoji", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.numseven.Location = new System.Drawing.Point(195, 199);
+            this.numseven.Name = "numseven";
+            this.numseven.Size = new System.Drawing.Size(126, 64);
+            this.numseven.TabIndex = 0;
+            this.numseven.Text = "7";
+            this.numseven.UseVisualStyleBackColor = false;
+            this.numseven.Click += new System.EventHandler(this.numseven_Click);
+            // 
+            // Input
+            // 
+            this.Input.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Input.Font = new System.Drawing.Font("Segoe UI Emoji", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.Input.Location = new System.Drawing.Point(195, 150);
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(547, 43);
+            this.Input.TabIndex = 1;
+            // 
+            // numeight
+            // 
+            this.numeight.BackColor = System.Drawing.SystemColors.Info;
+            this.numeight.Font = new System.Drawing.Font("Segoe UI Emoji", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.numeight.Location = new System.Drawing.Point(327, 199);
+            this.numeight.Name = "numeight";
+            this.numeight.Size = new System.Drawing.Size(126, 64);
+            this.numeight.TabIndex = 2;
+            this.numeight.Text = "8";
+            this.numeight.UseVisualStyleBackColor = false;
+            this.numeight.Click += new System.EventHandler(this.numeight_Click);
+            // 
+            // numnine
+            // 
+            this.numnine.BackColor = System.Drawing.SystemColors.Info;
+            this.numnine.Font = new System.Drawing.Font("Segoe UI Emoji", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.numnine.Location = new System.Drawing.Point(459, 199);
+            this.numnine.Name = "numnine";
+            this.numnine.Size = new System.Drawing.Size(133, 64);
+            this.numnine.TabIndex = 3;
+            this.numnine.Text = "9";
+            this.numnine.UseVisualStyleBackColor = false;
+            this.numnine.Click += new System.EventHandler(this.numnine_Click);
+            // 
+            // numfour
+            // 
+            this.numfour.BackColor = System.Drawing.SystemColors.Info;
+            this.numfour.Font = new System.Drawing.Font("Segoe UI Emoji", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.numfour.Location = new System.Drawing.Point(195, 263);
+            this.numfour.Name = "numfour";
+            this.numfour.Size = new System.Drawing.Size(126, 64);
+            this.numfour.TabIndex = 4;
+            this.numfour.Text = "4";
+            this.numfour.UseVisualStyleBackColor = false;
+            this.numfour.Click += new System.EventHandler(this.numfour_Click);
+            // 
+            // numfive
+            // 
+            this.numfive.BackColor = System.Drawing.SystemColors.Info;
+            this.numfive.Font = new System.Drawing.Font("Segoe UI Emoji", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.numfive.Location = new System.Drawing.Point(327, 263);
+            this.numfive.Name = "numfive";
+            this.numfive.Size = new System.Drawing.Size(126, 64);
+            this.numfive.TabIndex = 5;
+            this.numfive.Text = "5";
+            this.numfive.UseVisualStyleBackColor = false;
+            this.numfive.Click += new System.EventHandler(this.numfive_Click);
+            // 
+            // numsix
+            // 
+            this.numsix.BackColor = System.Drawing.SystemColors.Info;
+            this.numsix.Font = new System.Drawing.Font("Segoe UI Emoji", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.numsix.Location = new System.Drawing.Point(459, 263);
+            this.numsix.Name = "numsix";
+            this.numsix.Size = new System.Drawing.Size(133, 64);
+            this.numsix.TabIndex = 6;
+            this.numsix.Text = "6";
+            this.numsix.UseVisualStyleBackColor = false;
+            this.numsix.Click += new System.EventHandler(this.numsix_Click);
+            // 
+            // numone
+            // 
+            this.numone.BackColor = System.Drawing.SystemColors.Info;
+            this.numone.Font = new System.Drawing.Font("Segoe UI Emoji", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.numone.Location = new System.Drawing.Point(195, 333);
+            this.numone.Name = "numone";
+            this.numone.Size = new System.Drawing.Size(126, 64);
+            this.numone.TabIndex = 7;
+            this.numone.Text = "1";
+            this.numone.UseVisualStyleBackColor = false;
+            this.numone.Click += new System.EventHandler(this.numone_Click);
+            // 
+            // numtwo
+            // 
+            this.numtwo.BackColor = System.Drawing.SystemColors.Info;
+            this.numtwo.Font = new System.Drawing.Font("Segoe UI Emoji", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.numtwo.Location = new System.Drawing.Point(327, 333);
+            this.numtwo.Name = "numtwo";
+            this.numtwo.Size = new System.Drawing.Size(126, 64);
+            this.numtwo.TabIndex = 8;
+            this.numtwo.Text = "2";
+            this.numtwo.UseVisualStyleBackColor = false;
+            this.numtwo.Click += new System.EventHandler(this.numtwo_Click);
+            // 
+            // numthree
+            // 
+            this.numthree.BackColor = System.Drawing.SystemColors.Info;
+            this.numthree.Font = new System.Drawing.Font("Segoe UI Emoji", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.numthree.Location = new System.Drawing.Point(459, 333);
+            this.numthree.Name = "numthree";
+            this.numthree.Size = new System.Drawing.Size(133, 64);
+            this.numthree.TabIndex = 9;
+            this.numthree.Text = "3";
+            this.numthree.UseVisualStyleBackColor = false;
+            this.numthree.Click += new System.EventHandler(this.numthree_Click);
+            // 
+            // numzero
+            // 
+            this.numzero.BackColor = System.Drawing.SystemColors.Info;
+            this.numzero.Font = new System.Drawing.Font("Segoe UI Emoji", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.numzero.Location = new System.Drawing.Point(195, 403);
+            this.numzero.Name = "numzero";
+            this.numzero.Size = new System.Drawing.Size(126, 64);
+            this.numzero.TabIndex = 10;
+            this.numzero.Text = "0";
+            this.numzero.UseVisualStyleBackColor = false;
+            this.numzero.Click += new System.EventHandler(this.numzero_Click);
+            // 
+            // numsum
+            // 
+            this.numsum.BackColor = System.Drawing.SystemColors.Info;
+            this.numsum.Font = new System.Drawing.Font("Segoe UI Emoji", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.numsum.Location = new System.Drawing.Point(459, 403);
+            this.numsum.Name = "numsum";
+            this.numsum.Size = new System.Drawing.Size(133, 64);
+            this.numsum.TabIndex = 11;
+            this.numsum.Text = "=";
+            this.numsum.UseVisualStyleBackColor = false;
+            this.numsum.Click += new System.EventHandler(this.numsum_Click);
+            // 
+            // numpush
+            // 
+            this.numpush.BackColor = System.Drawing.SystemColors.Info;
+            this.numpush.Font = new System.Drawing.Font("Segoe UI Emoji", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.numpush.Location = new System.Drawing.Point(598, 199);
+            this.numpush.Name = "numpush";
+            this.numpush.Size = new System.Drawing.Size(144, 64);
+            this.numpush.TabIndex = 12;
+            this.numpush.Text = "+";
+            this.numpush.UseVisualStyleBackColor = false;
+            this.numpush.Click += new System.EventHandler(this.numpush_Click);
+            // 
+            // numminus
+            // 
+            this.numminus.BackColor = System.Drawing.SystemColors.Info;
+            this.numminus.Font = new System.Drawing.Font("Segoe UI Emoji", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.numminus.Location = new System.Drawing.Point(598, 263);
+            this.numminus.Name = "numminus";
+            this.numminus.Size = new System.Drawing.Size(144, 64);
+            this.numminus.TabIndex = 13;
+            this.numminus.Text = "-";
+            this.numminus.UseVisualStyleBackColor = false;
+            this.numminus.Click += new System.EventHandler(this.numminus_Click);
+            // 
+            // nummultiply
+            // 
+            this.nummultiply.BackColor = System.Drawing.SystemColors.Info;
+            this.nummultiply.Font = new System.Drawing.Font("Segoe UI Emoji", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.nummultiply.Location = new System.Drawing.Point(598, 333);
+            this.nummultiply.Name = "nummultiply";
+            this.nummultiply.Size = new System.Drawing.Size(144, 64);
+            this.nummultiply.TabIndex = 14;
+            this.nummultiply.Text = "*";
+            this.nummultiply.UseVisualStyleBackColor = false;
+            this.nummultiply.Click += new System.EventHandler(this.nummultiply_Click);
+            // 
+            // numdivision
+            // 
+            this.numdivision.BackColor = System.Drawing.SystemColors.Info;
+            this.numdivision.Font = new System.Drawing.Font("Segoe UI Emoji", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.numdivision.Location = new System.Drawing.Point(598, 403);
+            this.numdivision.Name = "numdivision";
+            this.numdivision.Size = new System.Drawing.Size(144, 64);
+            this.numdivision.TabIndex = 15;
+            this.numdivision.Text = "/";
+            this.numdivision.UseVisualStyleBackColor = false;
+            this.numdivision.Click += new System.EventHandler(this.numdivision_Click);
+            // 
+            // delete
+            // 
+            this.delete.BackColor = System.Drawing.Color.Red;
+            this.delete.Font = new System.Drawing.Font("Segoe UI Emoji", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.delete.Location = new System.Drawing.Point(760, 199);
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(144, 64);
+            this.delete.TabIndex = 16;
+            this.delete.Text = "C";
+            this.delete.UseVisualStyleBackColor = false;
+            this.delete.Click += new System.EventHandler(this.delete_Click);
+            // 
+            // point
+            // 
+            this.point.BackColor = System.Drawing.SystemColors.Info;
+            this.point.Font = new System.Drawing.Font("Segoe UI Emoji", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.point.Location = new System.Drawing.Point(327, 403);
+            this.point.Name = "point";
+            this.point.Size = new System.Drawing.Size(126, 64);
+            this.point.TabIndex = 17;
+            this.point.Text = ".";
+            this.point.UseVisualStyleBackColor = false;
+            this.point.Click += new System.EventHandler(this.point_Click);
+            // 
+            // process
+            // 
+            this.process.Font = new System.Drawing.Font("Segoe UI Emoji", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.process.Location = new System.Drawing.Point(195, 115);
+            this.process.Multiline = true;
+            this.process.Name = "process";
+            this.process.Size = new System.Drawing.Size(265, 29);
+            this.process.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(977, 505);
+            this.Controls.Add(this.process);
+            this.Controls.Add(this.point);
+            this.Controls.Add(this.delete);
+            this.Controls.Add(this.numdivision);
+            this.Controls.Add(this.nummultiply);
+            this.Controls.Add(this.numminus);
+            this.Controls.Add(this.numpush);
+            this.Controls.Add(this.numsum);
+            this.Controls.Add(this.numzero);
+            this.Controls.Add(this.numthree);
+            this.Controls.Add(this.numtwo);
+            this.Controls.Add(this.numone);
+            this.Controls.Add(this.numsix);
+            this.Controls.Add(this.numfive);
+            this.Controls.Add(this.numfour);
+            this.Controls.Add(this.numnine);
+            this.Controls.Add(this.numeight);
+            this.Controls.Add(this.Input);
+            this.Controls.Add(this.numseven);
+            this.Name = "Form1";
+            this.RightToLeftLayout = true;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button numseven;
+        private TextBox Input;
+        private Button numeight;
+        private Button numnine;
+        private Button numfour;
+        private Button numfive;
+        private Button numsix;
+        private Button numone;
+        private Button numtwo;
+        private Button numthree;
+        private Button numzero;
+        private Button numsum;
+        private Button numpush;
+        private Button numminus;
+        private Button nummultiply;
+        private Button numdivision;
+        private Button delete;
+        private Button point;
+        private TextBox process;
+    }
+}
---
+++ Lab01OOP/Lab01OOP/Form1.cs
@@ -0,0 +1,190 @@
+﻿namespace Lab01OOP
+{
+    public partial class Form1 : Form
+    {
+        // class variable
+        double input1, result = 0;
+        double input2;
+        string operation = "";
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // ปุ่ม =
+        private void numsum_Click(object sender, EventArgs e)
+        {
+            input2 = Double.Parse(this.Input.Text);
+            this.Input.Text = result.ToString("#,##0.#####");
+            this.process.Text = input1 + operation + input2 + " =";
+
+            if (this.operation == "+")
+            {
+                result = input1 + input2;
+                this.Input.Text =result.ToString("#,##0.#####");
+
+            }
+            else if(this.operation == "/")
+            {
+                result = input1 / input2;
+                this.Input.Text =result.ToString("#,##0.#####");
+
+            }
+            else if (this.operation == "*")
+            {
+                result = input1 * input2;
+                this.Input.Text =result.ToString("#,##0.#####");
+
+            }
+            else if (this.operation == "-")
+            {
+                result = input1 - input2;
+                this.Input.Text =result.ToString("#,##0.#####");
+
+            }
+            this.Input.Text = result.ToString("#,##0.#####");
+
+        }
+        // ปุ่ม+
+            private void numpush_Click(object sender, EventArgs e)
+        {
+            this.input1 = Double.Parse(this.Input.Text);
+            this.Input.Text = "";
+            this.operation = "+";
+            this.process.Text = this.process.Text + "+";
+
+        }
+        // ปุ่ม /
+        private void numdivision_Click(object sender, EventArgs e)
+        {
+            this.input1 = Double.Parse(this.Input.Text);
+            this.Input.Text = "";
+            this.operation = "/";
+            this.process.Text = this.process.Text + "/";
+
+        }
+        // ปุ่ม *
+        private void nummultiply_Click(object sender, EventArgs e)
+        {
+            this.input1 = Double.Parse(this.Input.Text);
+            this.Input.Text = "";
+            this.operation = "*";
+            this.process.Text = this.process.Text + "*";
+
+        }
+        // ปุ่ม -
+        private void numminus_Click(object sender, EventArgs e)
+        {
+            this.input1 = Double.Parse(this.Input.Text);
+            this.Input.Text = "";
+            this.operation = "-";
+            this.process.Text = this.process.Text + "-";
+
+        }
+        // ปุ่ม 0
+        private void numzero_Click(object sender, EventArgs e)
+        {
+
+            this.Input.Text = this.Input.Text + "0";
+            this.process.Text = this.process.Text + "0";
+
+        }
+        // ปุ่ม 1
+        private void numone_Click(object sender, EventArgs e)
+        {
+
+            this.Input.Text = this.Input.Text + "1";
+            this.process.Text = this.process.Text + "1";
+
+        }
+        // ปุ่ม 2
+        private void numtwo_Click(object sender, EventArgs e)
+        {
+
+            this.Input.Text = this.Input.Text + "2";
+            this.process.Text = this.process.Text + "2";
+
+
+
+        }
+        // ปุ่ม 3
+        private void numthree_Click(object sender, EventArgs e)
+        {
+
+            this.Input.Text = this.Input.Text + "3";
+            this.process.Text = this.process.Text + "3";
+
+
+        }
+        // ปุ่ม 4
+        private void numfour_Click(object sender, EventArgs e)
+        {
+
+            this.Input.Text = this.Input.Text + "4";
+            this.process.Text = this.process.Text + "4";
+
+
+        }
+        // ปุ่ม 5
+        private void numfive_Click(object sender, EventArgs e)
+        {
+
+            this.Input.Text = this.Input.Text + "5";
+            this.process.Text = this.process.Text + "5";
+
+
+        }
+        // ปุ่ม 6
+        private void numsix_Click(object sender, EventArgs e)
+        {
+
+            this.Input.Text = this.Input.Text + "6";
+            this.process.Text = this.process.Text + "6";
+
+
+        }
+        // ปุ่ม 7
+        private void numseven_Click(object sender, EventArgs e)
+        {
+
+            this.Input.Text = this.Input.Text + "7";
+            this.process.Text = this.process.Text + "7";
+
+
+        }
+        // ปุ่ม 8
+        private void numeight_Click(object sender, EventArgs e)
+        {
+
+            this.Input.Text = this.Input.Text + "8";
+            this.process.Text = this.process.Text + "8";
+
+
+        }
+        // ปุ่ม 9
+        private void numnine_Click(object sender, EventArgs e)
+        {
+
+            this.Input.Text = this.Input.Text + "9";
+            this.process.Text = this.process.Text + "9";
+
+
+        }
+
+        // ปุ่ม delete
+        private void delete_Click(object sender, EventArgs e)
+        {
+           this.Input.Text = "";
+            this.process.Text = "";
+        }
+
+        // ปุ่ม .
+        private void point_Click(object sender, EventArgs e)
+        {
+            this.Input.Text = this.Input.Text + ".";
+            this.process.Text = this.process.Text + ".";
+
+        }
+    }
+}
